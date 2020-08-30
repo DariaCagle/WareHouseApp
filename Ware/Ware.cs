166044cf523c87e2bc6d43042d9ff0d9e61686d9
DariaCagle <dani.merchard@gmail.com>
2020-08-30 22:08:52 +0300

    added ware class

---
+++ Ware/Ware.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareLib
+{
+    public class Ware
+    {
+        public string name;
+        public double price;
+        public string baseWare;
+        public DateTime manufacturingDate;
+
+        public DateTime ExpirationDate
+        {
+            get
+            {
+                switch (baseWare)
+                {
+                    case "bread":
+                        return manufacturingDate.AddDays(3);
+                    case "milk":
+                        return manufacturingDate.AddDays(10);
+                    case "butter":
+                        return manufacturingDate.AddDays(30);
+                }
+                return ExpirationDate;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {name}, price: {price}, expiration date: {ExpirationDate.ToShortDateString()}";
+        }
+
+    }
+}
---
+++ Ware/WareManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareLib
+{
+    public class WareManager
+    {
+        private Ware CreateWare(string name, double price, string baseWare, DateTime manufacturingDate)
+        {
+            Ware ware = new Ware();
+            ware.name = name;
+            ware.price = price;
+            ware.baseWare = baseWare;
+            ware.manufacturingDate = manufacturingDate;;
+            return ware;
+        }
+
+        public void CheckWare()
+        {
+            Ware w1 = CreateWare("White Milk", 19.50, "milk", new DateTime(2020, 08, 07));
+            Console.WriteLine(w1.ToString());
+
+            Ware w2 = CreateWare("Cereal Bread", 10.05, "bread", new DateTime(2020, 08, 15));
+            Console.WriteLine(w2.ToString());
+        }
+    }
+}
